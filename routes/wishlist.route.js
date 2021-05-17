f278c053e728fcae1fefdeff223e8663c1b29631
shivam <upadhyayshivam06@gmail.com>
2021-05-17 17:50:56 +0530

    fix:bug in wishlist api

--- routes/wishlist.route.js
+++ routes/wishlist.route.js
@@ -1,64 +1,59 @@
 const express = require("express");
 const wishlistRoute = express.Router();
-const {
-  getUserById
-} = require("../middlewares/user.find")
+const { getUserById } = require("../middlewares/user.find");
 wishlistRoute.param("userId", async (req, res, next, userId) => {
   try {
-    const user = await getUserById(userId,"wishlist");
+    const user = await getUserById(userId, "wishlist");
     req.user = user;
+    const { wishlist } = user;
+    const newWishlist = wishlist.map((item) => {
+      let items = item.productId;
+      return { ...items._doc };
+    });
+    req.wishlist = newWishlist;
     next();
   } catch (err) {
     res.json({
       success: false,
-      message: "user does not exists"
+      message: "user does not exists",
     });
   }
 });
-wishlistRoute.route("/:userId")
+wishlistRoute
+  .route("/:userId")
   .get(async (req, res) => {
     try {
-      const user = req.user;
-      const { wishlist } = await user;
-
-
-      let newWishlist=wishlist.map((item) => {
-        let items = item.productId;
-        return { ...items._doc, qnt: item.qnt };
-      })
-
+      const wishlist = req.wishlist;
       res.json({
         success: true,
-        wishlist:newWishlist
+        wishlist
       });
     } catch (err) {
       res.json({
         success: false,
-        message: "unable to get wishlist"
+        message: "unable to get wishlist",
       });
     }
   })
 
   .post(async (req, res) => {
     const { productId } = req.body;
+    console.log(productId);
     try {
-      const user =req.user;
-      let {wishlist}=user;
-      let present = wishlist.find(
-        (item) => item.productId == productId);
-      console.log(present)
-        if (!present) {
-        const newWishlist= { productId: productId };
+      const user = req.user;
+      let wishlist = req.wishlist;
+      let present = wishlist.find((item) => item._id == productId);
+      console.log(present);
+      if (!present) {
+        const newWishlist = { productId: productId };
         user.wishlist.push(newWishlist);
         await user.save();
-        res.json({ success: true, message:"added successfully" });
+        res.json({ success: true, message: "added successfully" });
       }
-      res.json({success:true,message:"added to wishlist"})
+      res.json({ success: true, message: "already in wishlist" });
     } catch (err) {
       console.log(err);
       res.json({ success: false, message: err });
     }
   });
-
-
 module.exports = wishlistRoute;
